556f5674c42be804c46675a8e7188391353d034e
Riku Tapper <riku.tapper@gmail.com>
2023-01-28 14:27:51 +0200

    uibuild

--- index.js
+++ index.js
@@ -12,129 +12,130 @@ const app = express()
 app.use(express.static('build'))
 app.use(express.json())
 
-morgan.token('reqdata', (req, res) => {
-    return JSON.stringify(req.body)
+morgan.token('reqdata', (req) => {
+  return JSON.stringify(req.body)
 })
 
 const custom_morgan = morgan((tokens, req, res) => {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        tokens.reqdata(req, res)
-    ].join(' ')
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.reqdata(req, res)
+  ].join(' ')
 })
 
 // POST /api/persons 200 61 - 4.896 ms {"name":"Liisa Marttinen", "number":"040-243563"}
 app.use(custom_morgan)
 
 app.get('/info', (req, res) => {
-    const date = new Date()
-    const options = {
-        weekday: 'short',
-        month: 'short',
-        day: '2-digit',
-        year: 'numeric',
-        timeZoneName: 'long',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    }
-    Person.find({})
-        .then(persons => {
-            res.send(`<p>Phonebook has info for ${persons.length} people</p>
+  const date = new Date()
+  const options = {
+    weekday: 'short',
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+    timeZoneName: 'long',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  }
+  Person.find({})
+    .then(persons => {
+      res.send(`<p>Phonebook has info for ${persons.length} people</p>
             <p>${date.toLocaleString('en-FI', options)}`)
-        })
+    })
 })
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
-        .then(persons => {
-            res.json(persons)
-        })
-        .catch(error => {
-            next(error)
-        })
+  Person.find({})
+    .then(persons => {
+      res.json(persons)
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findById(id)
-        .then(person => {
-            if (person) {
-                res.json(person)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => {
-            next(error)
-        })
+  const id = req.params.id
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findByIdAndRemove(id)
-        .then(person => {
-            res.status(204).end()
-        })
-        .catch(error => {
-            next(error)
-        })
+  const id = req.params.id
+  Person.findByIdAndRemove(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.post('/api/persons', (req, res, next) => {
-    if (!req.body.name || !req.body.number) {
-        return res.status(400).json({
-            error: 'name or number missing'
-        })
-    }
-    const newPerson = new Person({
-        name: req.body.name,
-        number: req.body.number
+  if (!req.body.name || !req.body.number) {
+    return res.status(400).json({
+      error: 'name or number missing'
     })
-    newPerson.save()
-        .then(person => {
-            res.json(person)
-        })
-        .catch(error => next(error))
+  }
+  const newPerson = new Person({
+    name: req.body.name,
+    number: req.body.number
+  })
+  newPerson.save()
+    .then(person => {
+      res.json(person)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    const person = {
-        name: req.body.name,
-        number: req.body.number
-    }
-    Person.findByIdAndUpdate(id, person, {new: true, runValidators: true, context: 'query'})
-        .then(newPerson => {
-            res.json(newPerson)
-        })
-        .catch(error => next(error))
+  const id = req.params.id
+  const person = {
+    name: req.body.name,
+    number: req.body.number
+  }
+  Person.findByIdAndUpdate(id, person, {new: true, runValidators: true, context: 'query'})
+    .then(newPerson => {
+      res.json(newPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'})
+  res.status(404).send({error: 'unknown endpoint'})
 }
 app.use(unknownEndpoint)
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err.message)
+  console.error(err.message)
 
-    if (err.name === 'CastError') {
-        return res.status(400).send({error: 'malformatted id'})
-    } else if (err.name === 'ValidationError') {
-        return res.status(400).send({error: err.message})
-    }
+  if (err.name === 'CastError') {
+    return res.status(400).send({error: 'malformatted id'})
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).send({error: err.message})
+  }
 
-    next(err)
+  next(err)
 }
 app.use(errorHandler)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
--- mongo.js
+++ mongo.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Give at least your password, and name and number as arguments if you are adding a person')
-    process.exit(1)
+  console.log('Give at least your password, and name and number as arguments if you are adding a person')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -15,40 +16,40 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Person.find({})
-          .then(res => {
-            res.forEach(p => {
-                console.log(p.name, p.number)
-                mongoose.connection.close()
-            })
-          })
-} else if (process.argv.length === 4) {
-    console.log('In addition to password, please give name AND number for new person.')
-    mongoose.connection.close()
-} else if (process.argv.length === 5) {
-    const name = process.argv[3]
-    const number = process.argv[4]
-
-    const person = new Person({
-        name: name,
-        number: number
-    })
-
-    person.save()
-      .then(res => {
-        console.log(`added ${name} number ${number} to phonebook`)
+  Person.find({})
+    .then(res => {
+      res.forEach(p => {
+        console.log(p.name, p.number)
         mongoose.connection.close()
       })
+    })
+} else if (process.argv.length === 4) {
+  console.log('In addition to password, please give name AND number for new person.')
+  mongoose.connection.close()
+} else if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number
+  })
+
+  person.save()
+    .then(() => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      mongoose.connection.close()
+    })
 } else {
-    console.log('Invalid nnumber of paramenters')
-    mongoose.connection.close()
+  console.log('Invalid nnumber of paramenters')
+  mongoose.connection.close()
 }
 
 
